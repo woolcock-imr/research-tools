5c500cf88e212c6a42cec7784ec9b10f6c0d0d19
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-05-08 10:45:13 +1000

    update

---
+++ copy.bat
@@ -0,0 +1,4 @@
+xcopy %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\apps\woolcock-data-management\pages\*.* %USERPROFILE%\Documents\GitHub\woolcock-imr\research-tools\pages\*.* /S /D
+xcopy %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\apps\woolcock-data-management\panels\data-management.html %USERPROFILE%\Documents\GitHub\woolcock-imr\research-tools\panels\data-management.html /S /D
+
+pause
